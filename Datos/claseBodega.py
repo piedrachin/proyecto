1f3fc2faa26eb13e63701db9d97f6d994cac5f14
piedrachin <123708567+piedrachin@users.noreply.github.com>
2023-03-05 15:51:14 -0600

    cliente

--- Datos/claseBodega.py
+++ Datos/claseBodega.py
@@ -2,8 +2,8 @@
 # que materiales tendra y marca
 # y tambien la creacion de una nueva bodega de ser necesaria
 class Bodega:
-    def __init__(self, materiales, marca, tipo_bodega, crear_bodega) -> None:
-        self.materiales = materiales
-        self.marca = marca
+    def __init__(self, materiales, distribuidor, tipo_bodega, crear_bodega) -> None:
+        self.materiales = materiales # que tipo de materiales contendra mi bodega
+        self.distribuidor = distribuidor # Quien me distribuye los materiales de mi bodega
         self.tipo_bodega = tipo_bodega # este metodo es para diferenciar mi bodega
         self.crear_bodega = crear_bodega # me permitira crear una bodega
