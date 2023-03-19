e215eade7a4bcd07f3042e6d226c1aaa8f6ccf1c
piedrachin <123708567+piedrachin@users.noreply.github.com>
2023-03-19 14:14:28 -0600

    cambios

---
+++ Aplicacion/registro_articulos.py
@@ -0,0 +1,6 @@
+# esta clase se encargara de guardar mis articulos en una especie de registro
+# importando los metodos desde mi carpeta Datos, concernientes a la clase Articulo donde se almacena
+# toda la informacion respecto a como se podria ingresar un articulo.
+
+
+
